aab276db35aaf3c041a17f845312730e0b698ce9
shreyas-bk <56465925+shreyas-bk@users.noreply.github.com>
2020-07-08 11:55:52 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# OptimalCluster
+OptimalCluster is the Python implementation of various algorithms to find the optimal number of clusters. The algorithms include elbow, elbow-k_factor, silhouette, gap statistics, gap statistics with standard error, and gap statistics without log.
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="OptimalCluster",
+    version="0.0.1",
+    author="Shreyas Kera",
+    author_email="shreykera7@gmail.com",
+    description="Python implementation of various algorithms to find the optimal number of clusters",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shreyas-bk/OptiClustTest",
+    packages=['OptimalCluster'],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
