5ddd7afdc494fa4a94a7bade9115b782d07e55b5
shreyas-bk <56465925+shreyas-bk@users.noreply.github.com>
2020-07-13 11:48:01 +0530

    v0.1.0
    v0.1.0 added documentation and comments, changed default value of se_weight parameter in elbow_kf to 0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="OptimalCluster",
-    version="0.0.1",
+    version="0.1.0",
     author="Shreyas Kera",
     author_email="shreykera7@gmail.com",
     description="Python implementation of various algorithms to find the optimal number of clusters",
